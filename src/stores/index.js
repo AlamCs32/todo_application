17f2cb06731e11a0789a36ab6d8b43014081f141
AlamCs32 <skmdmehfoozalam@gmail.com>
2025-02-23 14:52:16 +0530

    Create Api Slices is Completed

--- src/stores/index.js
+++ src/stores/index.js
@@ -2,13 +2,14 @@ import { configureStore } from "@reduxjs/toolkit";
 import persistStore from "redux-persist/es/persistStore";
 import rootReducers from "./rootReducer";
 import { userApiAction } from "./apiSlice/userApiSlice";
+import { todoApiActions } from "./apiSlice/todoApiSlice";
 
 export const store = configureStore({
   reducer: rootReducers,
   middleware: (getDefaultMiddleware) =>
     getDefaultMiddleware({
       serializableCheck: false,
-    }).concat([userApiAction.middleware]),
+    }).concat([userApiAction.middleware, todoApiActions.middleware]),
 });
 
 export const persistor = persistStore(store);
